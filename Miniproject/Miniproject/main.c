46f6a0a51d547e1d5747f7fe31181d81605df305
swavans <sworms@avans.nl>
2017-03-23 15:24:25 +0100

    Update main.c
    IT WORKS

--- Miniproject/Miniproject/main.c
+++ Miniproject/Miniproject/main.c
@@ -1,9 +1,7 @@
-
 #define F_CPU 8000000UL // 8 MHz clock speed
-
 #include <avr/io.h>
 #include <util/delay.h>
-
+#define MARGE	15
 /*
 * init servo at the right output port
 */
@@ -12,87 +10,112 @@ void servo_init()
 	DDRC = 0x03; //Makes RC0&1 output pin
 	PORTC = 0x00;
 }
-
 /*
- *	tick one step left for the horizontal servo
- */
- void servoH_tick_left()
- {
+*	tick one step left for the horizontal servo
+*/
+void servoH_tick_left()
+{
 	PORTC = 0x01;
-	_delay_us(600);
+	_delay_us(500);
 	PORTC = 0x00;
- }
-
- /*
- * tick one step right for the horizontal servo
- */
- void servoH_tick_right()
- {
+}
+/*
+* tick one step right for the horizontal servo
+*/
+void servoH_tick_right()
+{
 	PORTC = 0x01;
-	_delay_us(2250);
+	_delay_us(2400);
 	PORTC = 0x00;
- }
-
- /*
- *tick one step up for the vertical servo
- */
- void servoV_tick_up()
- {
+}
+/*
+*tick one step up for the vertical servo
+*/
+void servoV_tick_up()
+{
 	PORTC = 0x02;
 	_delay_us(2250);
 	PORTC = 0x00;
- }
-
- /*
- *
- */
- void servoV_tick_down()
- {
+}
+/*
+*
+*/
+void servoV_tick_down()
+{
 	PORTC = 0x02;
 	_delay_us(600);
 	PORTC = 0x00;
- }
+}
 
 
 int main(void)
 {
+	DDRA	= 0xFF;
+	DDRB	= 0xFF;
+	DDRD	= 0xFF;
+	DDRE	= 0xFF;
+	DDRF	= 0x00;
+	ADCSRA	= 0b11100110;
+
+	
 	servo_init();
 	while(1)
 	{
-		////Rotate Motor to 0 degree
-		//PORTC = 0x01;
-		//_delay_us(1000);
-		//PORTC = 0x00;
-//
-		//_delay_ms(2000);
-
-		////Rotate Motor helemaal naar links
-		//PORTC = 0x03;
-		//_delay_us(600);
-		//PORTC = 0x00;
-//
-		//_delay_ms(2000);
-//
-		////Rotate Motor helemaal naar rechts
-		//PORTC = 0x03;
-		//_delay_us(2250);
-		//PORTC = 0x00;
-
-		for(int i =0; i<10; i++)
-		{
-			servoV_tick_down();
-			servoH_tick_left();
-			_delay_ms(10);
-		}
+		int sensors[4];
+		ADMUX = 0b01100000;
+		_delay_ms(50);
+		sensors[0] = ADCL+ADCH;
 		
-		_delay_ms(2000);
+		ADMUX = 0b01100001;
+		_delay_ms(50);
+		sensors[1] = ADCL+ADCH;
 
-		for(int i=0; i<10; i++)
-		{		
-			servoV_tick_up();
+		ADMUX = 0b01100010;
+		_delay_ms(50);
+		sensors[2] = ADCL+ADCH;
+		
+		ADMUX = 0b01100011;
+		_delay_ms(50);
+		sensors[3] = ADCL+ADCH;
+		
+		if((sensors[0]+sensors[1])>(sensors[2]+sensors[3]+MARGE))
+		{
+			PORTA=sensors[0];
+			PORTB=sensors[1];
+			PORTD=sensors[2];
+			PORTE=sensors[3];
 			servoH_tick_right();
 			_delay_ms(10);
 		}
-		_delay_ms(2000);
+		else if((sensors[2]+sensors[3])>(sensors[0]+sensors[1]+MARGE))
+		{
+			PORTA=sensors[0];
+			PORTB=sensors[1];
+			PORTD=sensors[2];
+			PORTE=sensors[3];
+			servoH_tick_left();
+			_delay_ms(100);
+		}
+
+		if((sensors[0]+sensors[3])>(sensors[1]+sensors[2]+MARGE))
+		{
+			PORTA=sensors[0];
+			PORTB=sensors[1];
+			PORTD=sensors[2];
+			PORTE=sensors[3];
+			servoV_tick_up();
+			_delay_ms(10);
+		}
+		else if((sensors[1]+sensors[2])>(sensors[0]+sensors[3]+MARGE))
+		{
+			PORTA=sensors[0];
+			PORTB=sensors[1];
+			PORTD=sensors[2];
+			PORTE=sensors[3];
+			servoV_tick_down();
+			_delay_ms(100);
+		}
+		
+		_delay_ms(500);
 	}
 }
