9e7283cdeca998f7ee96a9d239387fe62cd1ce4b
RickvF <pajfesse@avans.nl>
2017-03-29 11:24:03 +0200

    Miniproject lichtvolger

--- Miniproject/Miniproject/main.c
+++ Miniproject/Miniproject/main.c
@@ -1,98 +1,117 @@
-
-#define F_CPU 8000000UL // 8 MHz clock speed
-
+#define F_CPU 8000000UL
 #include <avr/io.h>
 #include <util/delay.h>
 
-/*
-* init servo at the right output port
-*/
-void servo_init()
+#define MARGE 200
+int servoh = 46;
+int servov = 46;
+
+void servo_init( void )
 {
-	DDRC = 0x03; //Makes RC0&1 output pin
-	PORTC = 0x00;
+	OCR1A = 46;					// RED, default, off
+	OCR1B = 46;					// GREEN, default, off
+	OCR1C = 46;					// BLUE, default, off
+	TCCR1A = 0b10101001;		// compare output OC1A,OC1B,OC1C
+	TCCR1B = 0b00001100;		// fast PWM 8 bit, prescaler=64, RUN
 }
 
-/*
- *	tick one step left for the horizontal servo
- */
- void servoH_tick_left()
- {
-	PORTC = 0x01;
-	_delay_us(600);
-	PORTC = 0x00;
- }
-
- /*
- * tick one step right for the horizontal servo
- */
- void servoH_tick_right()
- {
-	PORTC = 0x01;
-	_delay_us(2250);
-	PORTC = 0x00;
- }
-
- /*
- *tick one step up for the vertical servo
- */
- void servoV_tick_up()
- {
-	PORTC = 0x02;
-	_delay_us(2250);
-	PORTC = 0x00;
- }
-
- /*
- *
- */
- void servoV_tick_down()
- {
-	PORTC = 0x02;
-	_delay_us(600);
-	PORTC = 0x00;
- }
-
+void setServo( unsigned char servoV, unsigned char servoH )
+{
+	OCR1A = servoV;
+	OCR1B = servoH;
+}
 
 int main(void)
 {
+	DDRB	= 0xFF; //servo
+	DDRA	= 0xFF;
+	DDRC	= 0xFF;
+	DDRD	= 0xFF;
+	DDRE	= 0xFF;
+	DDRF	= 0x00;
+	ADCSRA	= 0b11100110;
 	servo_init();
-	while(1)
+	_delay_ms(250);
+	int pos = 30;
+
+	//setRed(46);//30 links //46 midden //62 meest recht
+	while (1)
 	{
-		////Rotate Motor to 0 degree
-		//PORTC = 0x01;
-		//_delay_us(1000);
-		//PORTC = 0x00;
-//
-		//_delay_ms(2000);
-
-		////Rotate Motor helemaal naar links
-		//PORTC = 0x03;
-		//_delay_us(600);
-		//PORTC = 0x00;
-//
-		//_delay_ms(2000);
-//
-		////Rotate Motor helemaal naar rechts
-		//PORTC = 0x03;
-		//_delay_us(2250);
-		//PORTC = 0x00;
-
-		for(int i =0; i<10; i++)
-		{
-			servoV_tick_down();
-			servoH_tick_left();
-			_delay_ms(10);
-		}
+		int sensors[4];
+		ADMUX = 0b01100000;
+		_delay_ms(50);
+		sensors[0] = ADCL+ADCH;
 		
-		_delay_ms(2000);
+		ADMUX = 0b01100001;
+		_delay_ms(50);
+		sensors[1] = ADCL+ADCH;
 
-		for(int i=0; i<10; i++)
-		{		
-			servoV_tick_up();
-			servoH_tick_right();
-			_delay_ms(10);
+		ADMUX = 0b01100010;
+		_delay_ms(50);
+		sensors[2] = ADCL+ADCH;
+		
+		ADMUX = 0b01100011;
+		_delay_ms(50);
+		sensors[3] = ADCL+ADCH;
+
+		PORTA=sensors[0];
+		PORTC=sensors[1];
+		PORTD=sensors[2];
+		PORTE=sensors[3];
+
+		int up = sensors[0] + sensors[3];
+		int down = sensors[1] + sensors[2];
+		int left = sensors[0] + sensors[1];
+		int right = sensors[2] + sensors[3];
+
+		if(up > down)
+		{
+			if(up-down>MARGE)
+			{
+				servov = servov+1;
+				if(servov > 61)
+				{
+					servov = 61;
+				}
+			}
 		}
-		_delay_ms(2000);
+		else if(up < down)
+		{
+			if(down-up>MARGE)
+			{
+				servov = servov-1;
+				if(servov < 30)
+				{
+					servov = 30;
+				}
+			}
+		}
+
+		if(left > right)
+		{
+			if(left-right>MARGE)
+			{
+				servoh = servoh+1;
+				if(servoh > 62)
+				{
+					servoh = 62;
+				}
+			}
+		}
+		else if(left < right)
+		{
+			if(right-left>MARGE)
+			{
+				servoh = servoh-1;
+				if(servoh < 30)
+				{
+					servoh = 30;
+				}
+			}
+		}
+
+		setServo(servov, servoh);
 	}
 }
+
+
